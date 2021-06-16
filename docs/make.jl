0e8e8b4cab6ab148a7089065af8bd7fc54ad70b5
Joshua Laughner <jlaugh@caltech.edu>
2021-06-16 12:35:14 -0400

    Added workflow for deploying documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,8 @@
 using Documenter, HSSModel;
 
 makedocs(sitename="HSSModel");
+if length(ARGS) > 0 && ARGS[1] == "deploy"
+    deploydocs(repo = "github.com/joshua-laughner/HSSModel.git",)
+else
+    println("To deploy to gh-pages, add `deploy` as a command line argument")
+end
